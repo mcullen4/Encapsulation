ee749df80fa7c4bcabd5df041dc953084c2b6372
Michele <Michele@Michele-PC>
2013-09-05 08:15:12 -0500

    Start of class construction for lab 2

---
+++ src/lab2/GameCharacter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Michele
+ */
+public class GameCharacter {
+   
+   private String name;
+   private int playerNumber;
+   private String pieceDescription;
+   private int gameSpace;
+   
+   
+   
+   public void takeTurn(){};
+   private void pickCard(){};
+   private void rollDice(){};
+   private void moveSpace(){};
+}
---
+++ src/lab2/StartUp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Michele
+ */
+public class StartUp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
